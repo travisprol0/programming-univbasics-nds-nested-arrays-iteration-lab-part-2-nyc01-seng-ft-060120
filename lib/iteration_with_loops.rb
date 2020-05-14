2474bb92418731c2a1f0814f9e33b69b06fe8199
Travis Prol <travisjohnprol@gmail.com>
2020-05-14 19:34:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@
     row_index = 0
       while row_index < src.count do
         element_index = 0
-        min_integer_element = 0
+        min_integer_element = 100
           while element_index < src[row_index].count do
             if src[row_index][element_index] < min_integer_element
               min_integer_element = src[row_index][element_index]
